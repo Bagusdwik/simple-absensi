b7b1f6922b2d6c94a724e858aa1a0b60ded49b29
Bagusdwik <dwibaguskurniawan07@gmail.com>
2023-03-12 21:51:56 +0700

    second commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asus/simple-absensi',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3fa0df8e58387b3381cd7abf217f2964915b0e05',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'asus/simple-absensi' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3fa0df8e58387b3381cd7abf217f2964915b0e05',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
